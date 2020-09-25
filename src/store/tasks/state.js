8168741febe023ae39ccc917282f1020a0d59f6a
Thayana Mamoré <thayanacmamore@gmail.com>
2020-09-25 00:42:26 -0300

    feat: transformando a tasks store (state) em objeto, visto que o Firebase não usa array, mas sim somente objetos

--- src/store/tasks/state.js
+++ src/store/tasks/state.js
@@ -1,27 +1,24 @@
 export default function () {
   return {
-    tasks: [
-      {
-        id: 1,
+    tasks: {
+      ID1: {
         name: 'Arrumar quarto',
         completed: false,
         dueDate: '2020/09/24',
         dueTime: '18:30'
       },
-      {
-        id: 2,
+      ID2: {
         name: 'Tomar café da manhã',
         completed: false,
         dueDate: '2020/09/25',
         dueTime: '10:30'
       },
-      {
-        id: 3,
+      ID3: {
         name: 'Dar banho nos cachorros',
         completed: false,
         dueDate: '2020/09/26',
         dueTime: '19:00'
       }
-    ]
+    }
   }
 }
