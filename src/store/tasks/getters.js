76306439eb5f908c4a3cefbe57df74cadb3c7c06
Thayana Mamoré <thayanacmamore@gmail.com>
2020-09-25 00:33:06 -0300

    feat: iniciada utilização do Vuex

---
+++ src/store/tasks/getters.js
@@ -0,0 +1,3 @@
+export function tasks ({ tasks }) {
+  return tasks
+}
---
+++ src/store/tasks/state.js
@@ -0,0 +1,27 @@
+export default function () {
+  return {
+    tasks: [
+      {
+        id: 1,
+        name: 'Arrumar quarto',
+        completed: false,
+        dueDate: '2020/09/24',
+        dueTime: '18:30'
+      },
+      {
+        id: 2,
+        name: 'Tomar café da manhã',
+        completed: false,
+        dueDate: '2020/09/25',
+        dueTime: '10:30'
+      },
+      {
+        id: 3,
+        name: 'Dar banho nos cachorros',
+        completed: false,
+        dueDate: '2020/09/26',
+        dueTime: '19:00'
+      }
+    ]
+  }
+}
